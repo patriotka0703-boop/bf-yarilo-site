4943de09a60ab398d86fb122b1cede393ec27e31
patriotka0703-boop <patriotka0703@gmail.com>
2026-09-15 13:42:03 +0300

    Create config.js

---
+++ admin/config.js
@@ -0,0 +1,23 @@
+window.YARILO_ADMIN = {
+  siteName: "Благотворительный фонд ЯРИЛО",
+  siteUrl: "https://bf-yarilo.ru",
+
+  pages: [
+    { name: "Главная", file: "index.html" },
+    { name: "О фонде", file: "about.html" },
+    { name: "Помощь", file: "help.html" },
+    { name: "Проекты", file: "projects.html" },
+    { name: "Новости", file: "news.html" },
+    { name: "Отчёты", file: "reports.html" },
+    { name: "Реквизиты", file: "requisites.html" },
+    { name: "Контакты", file: "contacts.html" },
+    { name: "Пожертвования", file: "donate.html" }
+  ],
+
+  seo: {
+    title: "Благотворительный фонд ЯРИЛО",
+    description: "Официальный сайт Благотворительного фонда ЯРИЛО",
+    sitemap: "/sitemap.xml",
+    robots: "/robots.txt"
+  }
+};
